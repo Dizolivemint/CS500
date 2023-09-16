7cfbcf75de04e2fd24929cd8afdebd70c7999a28
Miles Exner <miles.exner@csuglobal.edu>
2023-09-15 22:10:43 -0700

    🥅 add error catching

--- Module 1/Assignment/add_subtract.py
+++ Module 1/Assignment/add_subtract.py
@@ -1,17 +1,25 @@
-# input two numbers
-num1 = input('Input the first number: ')
-num2 = input('Input the second number: ')
+def main():
+  # input two numbers
+  num1 = input('Input the first number: ')
+  num2 = input('Input the second number: ')
 
-# convert the strings into regular numbers
-num1 = int(num1)
-num2 = int(num2)
+  # convert the strings into regular numbers
+  try:
+    num1 = int(num1)
+    num2 = int(num2)
+  except ValueError:
+    print('You must enter a number.')
+    main()
+    exit()
 
-# add the numbers together
-add_total = num1 + num2
+  # add the numbers together
+  add_total = num1 + num2
 
-# subtract the numbers
-subtract_total = num1 - num2
+  # subtract the numbers
+  subtract_total = num1 - num2
 
-# output the results
-print(num1, '+', num2, '=', add_total)
-print(num1, '-', num2, '=', subtract_total)
+  # output the results
+  print(num1, '+', num2, '=', add_total)
+  print(num1, '-', num2, '=', subtract_total)
+  
+main()
--- Module 1/Assignment/multiply_divide.py
+++ Module 1/Assignment/multiply_divide.py
@@ -1,17 +1,25 @@
-# input two numbers
-num1 = input('Input the first number: ')
-num2 = input('Input the second number: ')
+def main():
+  # input two numbers
+  num1 = input('Input the first number: ')
+  num2 = input('Input the second number: ')
 
-# convert the strings into regular numbers
-num1 = int(num1)
-num2 = int(num2)
+  # convert the strings into regular numbers
+  try:
+    num1 = int(num1)
+    num2 = int(num2)
+  except ValueError():
+    print('You must enter a number.')
+    main()
+    exit()
 
-# multiply the numbers
-multiply_total = num1 * num2
+  # multiply the numbers
+  multiply_total = num1 * num2
 
-# divide the numbers
-divide_total = num1 / num2
+  # divide the numbers
+  divide_total = num1 / num2
 
-# output the results
-print(num1, '*', num2, '=', multiply_total)
-print(num1, '/', num2, '=', divide_total)
+  # output the results
+  print(num1, '*', num2, '=', multiply_total)
+  print(num1, '/', num2, '=', divide_total)
+  
+main()
