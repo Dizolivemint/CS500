209f0b503da53a1c63cbc4e23480925a7a0451c4
Miles Exner <miles.exner@csuglobal.edu>
2023-09-15 21:34:08 -0700

    🎉 first module, yay!

---
+++ Module 1/Assignment/multiply_divide.py
@@ -0,0 +1,17 @@
+# input two numbers
+num1 = input('Input the first number: ')
+num2 = input('Input the second number: ')
+
+# convert the strings into regular numbers
+num1 = int(num1)
+num2 = int(num2)
+
+# multiply the numbers
+multiply_total = num1 * num2
+
+# divide the numbers
+divide_total = num1 / num2
+
+# output the results
+print(num1, '*', num2, '=', multiply_total)
+print(num1, '/', num2, '=', divide_total)
