003a0e1b13a104a2f8a0adc7d7336b77ba274f11
Miles Exner <miles.exner@csuglobal.edu>
2023-11-03 17:02:53 -0700

    ♻️ update print method

--- Portfolio/shop/ItemToPurchase.py
+++ Portfolio/shop/ItemToPurchase.py
@@ -7,5 +7,7 @@ class ItemToPurchase:
     
   def print_item_cost(self):
     item_total_cost = self.item_quantity * self.item_price
-    print(item_total_cost)
+    price = f"${self.item_price:.2f}"
+    total = f"${item_total_cost:.2f}"
+    print(f"{self.item_name} * {self.item_quantity} @ {price} = {total}")
     return item_total_cost
